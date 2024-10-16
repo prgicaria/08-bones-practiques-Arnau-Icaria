08076e63c07563d59d14875409ad8c181632f303
ArnauIcaria <aroca-cusachs@instituticaria.cat>
2024-10-16 14:08:57 +0200

    primer enviament

---
+++ bones_practiques.py
@@ -0,0 +1,23 @@
+#!usr/bin/env python
+
+'''Divisió entera. Aquest programa fa una divisió entera de dos números
+
+Institut Icària
+Programació - 1r Batxillerat - Curs 2023-24
+
+Aquest programa demana dos números: dividend i divisor,
+  fa una divisió entera dels dos números entrats i mostra per pantalla la divisió, el quocient i el residu.
+'''
+
+__author__ = "Arnau Roca-Cusachs Valls"
+__email__ = "aroca-cusachs@instituticaria.cat"
+__date__ = "16/10/2024"
+
+dividend = int(input("Introdueix el dividend: "))
+divisor = int(input("Introdueix el divisor: "))
+quocient = dividend // divisor
+residu = dividend % divisor
+
+print (f"Divisió: {dividend}/{divisor}")
+print (f"Quocient: {quocient}")
+print (f"Residu: {residu}")
